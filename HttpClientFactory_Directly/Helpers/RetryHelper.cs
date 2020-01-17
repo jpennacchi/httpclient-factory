7be2fe1ebcc16584e47dc50b5a67733e48b49ef8
Julio Pennacchi <julio.pennacchi@dxtra-sw.com>
2020-01-17 13:38:20 -0300

    Implementing of retry pattern

---
+++ HttpClientFactory_Directly/Helpers/RetryHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HttpClientFactory_Directly.Helpers
+{
+    public static class RetryHelper
+    {
+        public static async Task RetryOnExceptionAsync(int times, int delay, Func<Task> operation)
+        {
+            var attempts = 0;
+
+            do
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine($"Attempt Times: {attempts}");
+                    await operation();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Request failed. More details: {ex.Message}.");
+
+                    if (attempts == times)
+                    {
+                        Console.WriteLine($"Max Attempt excceded :( ");
+                        throw;
+                    }
+
+                    Console.WriteLine($"Next Attempt in {delay / 1000} seconds.");
+                    await Task.Delay(delay);
+                }
+            } while (true);
+        }
+    }
+}
--- HttpClientFactory_Directly/Program.cs
+++ HttpClientFactory_Directly/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HttpClientFactory_Directly.Helpers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -39,30 +40,39 @@ namespace HttpClientFactory_Directly
         }
     }
 
-    public class MyHttpClientService : BackgroundService, IDisposable
+    public class MyHttpClientService : BackgroundService
     {
         private readonly HttpClient _client;
 
         public MyHttpClientService(IHttpClientFactory clientFactory)
         {
             _client = clientFactory.CreateClient();
-            _client.BaseAddress = new Uri("http://api.github.com");
+            _client.BaseAddress = new Uri("http://localhost:8089");
             _client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
             _client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var response = await GetData();
+            Console.WriteLine("Starting...");
 
-            Console.WriteLine($"Results: {response}");
+            await RetryHelper.RetryOnExceptionAsync(IntegrationConfig.MAX_RETRIES, IntegrationConfig.DELAY_RETRY, async () =>
+            {
+                var data = await GetData();
+                Console.WriteLine($"Resultado:  {data}.");
+            });
         }
 
         private async Task<string> GetData()
         {
-            
-            return await _client.GetStringAsync("/");
+            return await _client.GetStringAsync("/Edenred.Services/InvoiceQueue/Dequeue/ACCMM_AGENT/Rest");
         }
 
+    }
+
+    public static class IntegrationConfig
+    {
+        public const int MAX_RETRIES = 3;
+        public const int DELAY_RETRY = 1000;
     }
 }
